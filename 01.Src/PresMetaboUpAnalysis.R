bddb0af402784644284763122831b2f61e369181
ShawnWx2019 <shawnwang2016@126.com>
2023-07-14 18:17:24 +0800

    mrm.sh

---
+++ 01.Src/PresMetaboUpAnalysis.R
@@ -0,0 +1,592 @@
+#######################################################################
+#         Prj: MetMiner
+#         Assignment: UHPLC analysis of pseudotargeted metabolomics
+#         Author: Shawn Wang
+#         Init Date: Jan 03, 2023
+#         Update: Jul 14, 2023
+#######################################################################
+#> log: 
+#> Jul 14. fix bugs for ms2 selection.
+#> Jul 14. ready to push
+#> getopt
+TEST = "FALSE"
+options(stringsAsFactors = F)
+options(warn = -1)
+suppressMessages(if (!require('getopt')) BiocManager::install('getopt'))
+suppressMessages(if (!require('crayon')) BiocManager::install('crayon'))
+
+
+
+msg_yes = green$bold$italic
+msg_no = red$bold$italic
+msg_run = blue$bold$italic$underline
+msg_warning = yellow$bold$italic
+
+
+# args --------------------------------------------------------------------
+
+command=matrix(c(
+  'help', 'h', 0, 'logic', 'help information',
+  'MS1', 'x', 1, 'character', 'File Path of MS1". ',
+  'MS2', 'y', 1, 'character','File Path of MS2',
+  'sample_info', 'z', 1, 'character','sample information',
+  'column', 'c', 1, 'character','column type rp or hilic',
+  'ppm', 'm', 1, 'double', 'Related to m/z\n                         fluctuation of m/z value (ppm) from scan to scan - depends on the mass spectrometer accuracy\n                         eg:10',
+  'threads', 't', 1, 'integer', 'How many threads do you want to use.\n                          eg:20\n                          default:8',
+  'show_output', 'o', 2, 'Logic', 'Need tic, bpc and rt_correction_plot or not\n                          default: TRUE',
+  'p_min', 'a', 2, 'integer', 'Minimal of peakwidth for peak detection.\n                          eg:10\n                          default:10',
+  'p_max', 'b', 2, 'integer', 'Max of peakwidth for peak detection.\n                          eg:10\n                          default:10',
+  'min_fraction', 'f', 2, 'double', 'Related to Samples,\n                         to be valid, a group must be found in at least minFraction*n samples, with n=number of samples for each class of samples. A minFraction=0.5 corresponds to 50%.\n                         n=10, minFraction=0.5 => found in at least 5 samples\n                         default:0.5',
+  'QC_tag', 'g', 2, 'character', 'File path of QCn.mzXML file placed.\n                         eg: QC.mzXML files placed in NEG/QC, set -g "QC", QC.mzXML files placed in NEG/qc, set -g "qc"',
+  'snthresh', 's', 2, 'double', 'Related to intensity,\n                         signal/noise ratio threshold\n                         default: 5',
+  'noise', 'N', 2, 'double', 'Related to intensity,\n                         each centroid must be greater than the “noise” value\n                         default:500'
+),byrow = T, ncol = 5)
+args = getopt(command)
+
+## help information
+if (!is.null(args$help)) {
+  message(msg_yes(paste(getopt(command, usage = T), "\n")))
+  q(status=1)
+}
+
+## default value
+if (is.null(args$MS1)){
+  message(msg_no("-p error \nPlease fill in the file path of MS1 files correctly!"))
+  message(msg_yes(paste(getopt(command, usage = T), "\n")))
+  q(status = 1)
+}
+if (is.null(args$MS2)){
+  message(msg_no("-n error \nPlease fill in the file path of MS2 files correctly!"))
+  message(msg_yes(paste(getopt(command, usage = T), "\n")))
+  q(status = 1)
+}
+if (is.null(args$sample_info)){
+  message(msg_no("-n error \nPlease fill in the file path of sample_info files correctly!"))
+  message(msg_yes(paste(getopt(command, usage = T), "\n")))
+  q(status = 1)
+}
+if (is.null(args$QC_tag)){
+  message(msg_no("-g error \nPlease fill in the file path of QC files correctly!"))
+  message(msg_yes(paste(getopt(command, usage = T), "\n")))
+  q(status = 1)
+}
+
+if (is.null(args$ppm)){
+  args$ppm = 10
+}
+
+if (is.null(args$column)){
+  args$column = 'rp'
+}
+
+if (is.null(args$threads)){
+  args$threads = 8
+}
+
+if (is.null(args$snthresh)){
+  args$snthresh = 5
+}
+
+if (is.null(args$noise)){
+  args$noise = 500
+}
+
+if (is.null(args$min_fraction)){
+  args$min_fraction = 0.5
+}
+
+
+if (is.null(args$p_min)){
+  args$p_min = 10
+}
+
+if (is.null(args$p_max)){
+  args$p_max = 60
+}
+
+if (is.null(args$show_output)){
+  args$show_output = TRUE
+}
+
+
+# test --------------------------------------------------------------------
+library(tidymass)
+##test
+if (TEST == "TRUE") {
+  T.MS1 = "~/tmp/hpctest/presudo/MS1";
+  T.MS2 = "~/tmp/hpctest/presudo/MS2";
+  sample_info = "~/tmp/hpctest/presudo/sample_info.xlsx"
+  T.ppm = 10;
+  T.threads = 8;
+  T.show_output = FALSE;
+  T.p_min = 10;
+  T.p_max = 60;
+  T.min_fraction = 0.5;
+  T.QC_tag = "QC";
+  T.column = 'rp';
+  T.snthresh = 5;
+  T.noise = 500
+} else {
+  T.MS1 = args$MS1;
+  T.MS2 = args$MS2;
+  T.ppm = args$ppm;
+  sample_info = args$sample_info;
+  T.threads = args$threads;
+  T.show_output = args$show_output;
+  T.p_min = args$p_min;
+  T.p_max = args$p_max;
+  T.min_fraction = args$min_fraction;
+  T.QC_tag = args$QC_tag;
+  T.snthresh = args$snthresh;
+  T.noise = args$noise
+  T.column = args$column
+  ms1.pos <- paste0(T.MS1,"/POS/")
+  ms1.neg <- paste0(T.MS1,"/NEG/")
+  ms2.pos <- paste0(T.MS2,"/POS/")
+  ms2.neg <- paste0(T.MS2,"/NEG/")
+}
+#> 
+#> 
+suppressMessages(if (!require('devtools')) BiocManager::install('devtools'))
+suppressMessages(if (!require('tidyverse')) BiocManager::install('tidyverse'))
+suppressMessages(if (!require('patchwork')) BiocManager::install('patchwork'))
+suppressMessages(if (!require('bruceR')) install.packages("bruceR", dep=TRUE))
+suppressMessages(if (!require('MDAtoolkits')) install_github(repo = "ShawnWx2019/MDAtoolkits",ref = 'master'))
+suppressMessages(if (!require('MDAtoolkits')) install_github(repo = "ShawnWx2019/IMOtoolkits"))
+library(tidymass)
+
+
+# process data ------------------------------------------------------------
+
+message(msg_run("Step1. Peak peaking in positive model. This may take a long time. Please be patient!"))
+process_data(
+  path = ms1.pos,
+  polarity = "positive",
+  ppm = T.ppm,
+  threads = T.threads,
+  snthresh = T.snthresh,
+  noise = T.noise,
+  peakwidth = c(T.p_min,T.p_max),
+  output_tic = T.show_output,
+  output_bpc = T.show_output,
+  output_rt_correction_plot = T.show_output,
+  min_fraction = T.min_fraction,
+  fill_peaks = FALSE,
+  group_for_figure = T.QC_tag
+)
+
+##> NEG model
+##>
+message(msg_run("Step2. Peak peaking in negative model. This may take a long time. Please be patient!"))
+process_data(
+  path = ms1.neg,
+  polarity = "negative",
+  ppm = T.ppm,
+  threads = T.threads,
+  snthresh = T.snthresh,
+  noise = T.noise,
+  peakwidth = c(T.p_min,T.p_max),
+  output_tic = T.show_output,
+  output_bpc = T.show_output,
+  output_rt_correction_plot = T.show_output,
+  min_fraction = T.min_fraction,
+  fill_peaks = FALSE,
+  group_for_figure = T.QC_tag
+)
+
+message(msg_yes("Peak picking steps fininsed. Please check result in /NEG/Result or /POS/Result"))
+
+
+
+# functions ---------------------------------------------------------------
+##> theme
+
+theme1 =   theme(
+  panel.border = element_rect(size = 1.5),
+  axis.title = element_text(size = 14,color = 'black'),
+  axis.text = element_text(size = 12,color = 'black')
+)
+
+##> QC sample batch effect detection.
+batch_detect = function(object) {
+  plt_batch =
+    object %>%
+    extract_expression_data() %>%
+    dplyr::select(contains("QC")) %>%
+    pivot_longer(contains("QC"),values_to = "value",names_to = "QC_samples") %>%
+    drop_na() %>%
+    mutate(
+      value = log2(value),
+      QC_samples = paste0(
+        "QC",str_pad(
+          string = gsub("QC","",QC_samples),width = 3,side = 'left',pad = "0"
+        )
+      )
+    ) %>%
+    arrange(QC_samples) %>%
+    ggplot(data = .,mapping = aes(x = QC_samples,y = value,color = QC_samples))+
+    geom_boxplot()+
+    ylab("log2(Raw peak area)")+
+    xlab("")+
+    theme_bw()+
+    theme1+
+    theme(
+      axis.text.x = element_text(angle = 90,vjust = 1,hjust = 1),
+      legend.position = "none"
+    )
+  return(plt_batch)
+}
+
+## peak distribution
+peak_distribution <- function(object) {
+  plt_peak_dis =
+    object %>%
+    `+`(1) %>%
+    log(10) %>%
+    show_mz_rt_plot() +
+    scale_size_continuous(range = c(0.01, 2))+
+    theme1
+  return(plt_peak_dis)
+}
+extract_fragment <- function(ms2_data,vari_id,spec_id) {
+  var2spec = data.frame(
+    variable_id = vari_id,
+    ms2_spectrum_id = spec_id
+  )
+  out = map2_dfr(.x = spec_id,.y = ms2_data,.f = function(.x,.y) {
+    MS2_df <-
+      .y %>% 
+      as.data.frame() %>% 
+      mutate(
+        ms2_spectrum_id = .x
+      ) %>% 
+      relocate(ms2_spectrum_id,.before = mz)
+  })
+  mrm_out <- 
+  out %>% 
+    left_join(var2spec,by = 'ms2_spectrum_id') %>%
+    relocate(variable_id,.before = ms2_spectrum_id) %>% 
+    mutate(precursor = str_extract(ms2_spectrum_id,"(?<=mz).*(?=rt)") %>% as.numeric()) %>% 
+    group_by(variable_id) %>% 
+    arrange(variable_id,desc(intensity)) %>% 
+    mutate(fragment.order = 1:length(variable_id),
+           gap = precursor - mz) %>% 
+    filter(gap > 15) %>% 
+    slice_head(n = 1) %>% 
+    rename('product' = "mz") %>% 
+    select(variable_id,ms2_spectrum_id,precursor,product)
+  return(mrm_out)
+}
+oneStepMRMselection <- function(obj_ms2){
+  x = extract_ms2_data(obj_ms2)
+  x = x %>% setNames("MS2")
+  ms2_data <- x$MS2@ms2_spectra
+  vari_id <- x$MS2@variable_id
+  spec_id <- x$MS2@ms2_spectrum_id
+  res_mrm <- extract_fragment(ms2_data = ms2_data,vari_id = vari_id,spec_id = spec_id)
+  
+  ms2_tag <- data.frame(
+    variable_id = vari_id,
+    MS2_info = "yes"
+  )
+  object_new <- 
+    obj_ms2 %>% activate_mass_dataset('variable_info') %>%
+    left_join(ms2_tag,by = 'variable_id') %>% 
+    filter(MS2_info == 'yes') %>% 
+    left_join(res_mrm,by = 'variable_id') %>% 
+    drop_na()
+  return(object_new)
+  }
+##> working dir
+dir.create("workdir",showWarnings = F,recursive = T)
+# 1.0 positive model ----------------------------------------------------------
+
+##> import mass_dataset
+load(paste0(ms1.pos,"Result/object"))
+
+object.pos <- object
+##> load sample_info
+sample_info <- import(sample_info)
+object.pos <-
+  object.pos %>% 
+  activate_mass_dataset('sample_info') %>% 
+  select(sample_id) %>% left_join(sample_info)
+
+dir.create("workdir/01.raw",showWarnings = F,recursive = T)
+save(object.pos,file = "workdir/01.raw/object.pos")
+
+##> batch detection
+batch_detect_plt.pos <- 
+  batch_detect(object = object.pos)
+
+ggsave(filename = "workdir/01.raw/raw_peak_boxplot.pos.png",plot = batch_detect_plt.pos,width = 10,height = 5)
+ggsave(filename = "workdir/01.raw/raw_peak_boxplot.pos.pdf",plot = batch_detect_plt.pos,width = 10,height = 5)
+
+##> peak distribution
+
+peak_distribution_plt.pos <-
+  peak_distribution(object = object.pos)
+ggsave(filename = "workdir/01.raw/peak_distribution_plt.pos.png",plot = peak_distribution_plt.pos,width = 10,height = 5)
+ggsave(filename = "workdir/01.raw/peak_distribution_plt.pos.pdf",plot = peak_distribution_plt.pos,width = 10,height = 5)
+
+##> missing value
+plt_mv_raw.pos<-
+  show_sample_missing_values(object = object.pos, percentage = TRUE,color_by = 'group',order_by = 'injection.order')+theme1+
+  geom_hline(yintercept = 80,color = "red",linetype="dashed")+
+  ylim(c(0,100))+
+  scale_size_continuous(range = c(0.1, 2)) +
+  ggsci::scale_color_aaas()+
+  theme(axis.text.x = element_text(size = 3))
+
+ggsave(filename = "workdir/01.raw/missing_value_distribution.pos.png",plot = plt_mv_raw.pos,width = 10,height = 5)
+ggsave(filename = "workdir/01.raw/missing_value_distribution.pos.pdf",plot = plt_mv_raw.pos,width = 10,height = 5)
+
+# 1.2 Outlier detect and remove outliers ----------------------------------
+
+qc_id = object.pos %>%
+  activate_mass_dataset(what = "sample_info") %>%
+  filter(class == "QC") %>%
+  pull(sample_id)
+
+object.pos <-
+  object.pos %>%
+  mutate_variable_na_freq(according_to_samples = qc_id)
+##> na frequence is less than 0.2 in qc samples.
+object.pos.mv <-
+  object.pos %>%
+  activate_mass_dataset(what = "variable_info") %>%
+  filter(na_freq < 0.2)
+
+dir.create("workdir/02.remove_noise/",showWarnings = F,recursive = T)
+save(object.pos.mv,file = "workdir/02.remove_noise/object.pos.mv")
+
+plt_mv_remove_noise.pos<-
+  show_sample_missing_values(object = object.pos.mv, percentage = TRUE,color_by = 'group',order_by = 'injection.order')+theme1+
+  geom_hline(yintercept = 80,color = "red",linetype="dashed")+
+  ylim(c(0,100))+
+  theme(axis.text.x = element_text(size = 8,angle = 90)) +
+  scale_size_continuous(range = c(0.1, 2)) +
+  ggsci::scale_color_aaas()+
+  theme(axis.text.x = element_text(size = 3))
+
+ggsave(filename = "workdir/02.remove_noise/plt_mv_remove_noise.pos.png",plot = plt_mv_remove_noise.pos,width = 10,height = 5)
+ggsave(filename = "workdir/02.remove_noise/plt_mv_remove_noise.pos.pdf",plot = plt_mv_remove_noise.pos,width = 10,height = 5)
+
+outlier_samples.pos <-
+  object.pos.mv %>% 
+  `+`(1) %>% 
+  log(2) %>% 
+  scale() %>% 
+  detect_outlier(na_percentage_cutoff = 0.8)
+
+outlier_table.pos <-
+  extract_outlier_table(outlier_samples.pos)
+
+out_name.pos <-
+  outlier_table.pos %>%
+  filter(according_to_na == TRUE) %>% 
+  rownames()
+
+##> remove outlier based on 
+if(length(out_name.pos) != 0) {
+  object.pos.mv <- 
+    object.pos.mv %>% 
+    activate_mass_dataset('expression_data') %>% 
+    select(-all_of(out_name.pos))
+}
+
+plt_mv_remove_outlier.pos<-
+  show_sample_missing_values(object = object.pos.mv, percentage = TRUE,color_by = 'group',order_by = 'injection.order')+theme1+
+  geom_hline(yintercept = 80,color = "red",linetype="dashed")+
+  ylim(c(0,100))+
+  theme(axis.text.x = element_text(size = 8,angle = 90)) +
+  scale_size_continuous(range = c(0.1, 2)) +
+  ggsci::scale_color_aaas()+
+  theme(axis.text.x = element_text(size = 3))
+
+ggsave(filename = "workdir/02.remove_noise/plt_mv_remove_outlier.pos.png",plot = plt_mv_remove_outlier.pos,width = 10,height = 5)
+ggsave(filename = "workdir/02.remove_noise/plt_mv_remove_outlier.pos.pdf",plot = plt_mv_remove_outlier.pos,width = 10,height = 5)
+
+
+# impute mv ---------------------------------------------------------------
+object.pos.impute =
+  object.pos.mv %>%
+  impute_mv(method = 'knn')
+dir.create("workdir/03.impute_mv/",showWarnings = F,recursive = T)
+save(object.pos.impute,file = "workdir/03.impute_mv/object.pos.impute")
+
+
+# normalization -----------------------------------------------------------
+dir.create("workdir/04.normalization/",recursive = T,showWarnings = F)
+
+object_inte_pos <-
+  integrate_data(object.pos.impute,'qc_mean')
+save(object_inte_pos,file = "workdir/04.normalization/object_inte_pos")
+
+
+# rsd ---------------------------------------------------------------------
+
+rsd <- 
+  object_inte_pos %>% 
+  activate_mass_dataset("sample_info") %>%
+  dplyr::filter(class == T.QC_tag) %>%
+  extract_expression_data() %>%
+  rownames_to_column("ID") %>%
+  pivot_longer(contains(T.QC_tag),names_to = "tag",values_to = "value") %>%
+  select(-tag) %>%
+  group_by(ID) %>%
+  summarise(
+    raw.rsd = (sd(value,na.rm = T)/mean(value,na.rm = T))*100
+  ) %>% arrange(raw.rsd) %>% 
+  mutate(
+    check = case_when(
+      raw.rsd > 30 ~ "Failed",
+      TRUE ~ "PASS"
+    )
+  )
+
+rsd_check <- 
+  rsd %>% 
+  group_by(check) %>% 
+  summarise(num = n())
+
+writexl::write_xlsx(x = list(
+  rsd_val = rsd,
+  rsd_summary = rsd_check
+),"workdir/04.normalization/feature_rsd.pos.xlsx")
+
+
+# add MS2 -----------------------------------------------------------------
+#> add ms2 info
+object_pos.ms2 <-
+  object_inte_pos %>%
+  mutate_ms2(
+    object = .,
+    column = T.column,
+    polarity = 'positive',
+    ms1.ms2.match.mz.tol = 15,
+    ms1.ms2.match.rt.tol = 30,
+    path = ms2.pos
+  )
+#> extract features with ms2
+object_pos_MRM <- 
+  oneStepMRMselection(obj_ms2 = object_pos.ms2)
+
+# feature annotation ------------------------------------------------------
+
+##> load database 
+##> MS2
+db_file_path = dir("~/.HPC_tidymass/MS_db/MS_db/",pattern = "*.rda")
+purrr::map(.x = db_file_path,.f = function(.x){
+  load(paste0("~/.HPC_tidymass/MS_db/MS_db/",.x))
+})
+load("~/.HPC_tidymass/MS_db/MS_db/snyder_database_hilic0.0.3.rda")
+load("~/.HPC_tidymass/MS_db/MS_db/mona_database0.0.3.rda")
+load("~/.HPC_tidymass/MS_db/MS_db/hmdb_database0.0.3.rda")
+load("~/.HPC_tidymass/MS_db/MS_db/massbank_database0.0.3.rda")
+load("~/.HPC_tidymass/MS_db/MS_db/RIKEN_PlaSMA_database0.0.1.rda")
+load("~/.HPC_tidymass/MS_db/MS_db/plantcyc_ms1_database0.0.2.rda")
+load("~/.HPC_tidymass/MS_db/MS_db/KNApSAcK_ms1_database.rda")
+load("~/.HPC_tidymass/MS_db/MS_db/kegg_ms1_database0.0.3.rda")
+
+object_pos_anno <-
+  annotate_metabolites_mass_dataset(
+    object = object_pos_MRM,
+    polarity = 'positive',
+    ms1.match.ppm = 15,
+    column = T.column,
+    threads = 5,
+    database = kegg_ms1_database0.0.3,
+    candidate.num = 2
+  )
+
+object_pos_anno <-
+  annotate_metabolites_mass_dataset(
+    object = object_pos_anno,
+    polarity = 'positive',
+    ms1.match.ppm = 15,
+    column = T.column,
+    threads = 5,
+    database = plantcyc_ms1_database0.0.2,
+    candidate.num = 2
+  )
+
+object_pos_anno <-
+  annotate_metabolites_mass_dataset(
+    object = object_pos_anno,
+    polarity = 'positive',
+    ms1.match.ppm = 15,
+    column = T.column,
+    threads = 5,
+    database = KNApSAcK_ms1_database0.0.1,
+    candidate.num = 2
+  )
+# for ms2
+
+object_pos_anno <-
+  annotate_metabolites_mass_dataset(
+    object = object_pos_anno,
+    polarity = 'positive',
+    ms1.match.ppm = 15,
+    column = T.column,
+    threads = T.threads,
+    database = snyder_database_rplc0.0.3,
+    candidate.num = 2
+  )
+
+object_pos_anno <-
+  annotate_metabolites_mass_dataset(
+    object = object_pos_anno,
+    polarity = 'positive',
+    ms1.match.ppm = 15,
+    column = T.column,
+    threads = T.threads,
+    database = mona_database0.0.3,
+    candidate.num = 2
+  )
+
+object_pos_anno <-
+  annotate_metabolites_mass_dataset(
+    object = object_pos_anno,
+    polarity = 'positive',
+    ms1.match.ppm = 15,
+    column = T.column,
+    threads = T.threads,
+    database = massbank_database0.0.3,
+    candidate.num = 2
+  )
+
+object_pos_anno <-
+  annotate_metabolites_mass_dataset(
+    object = object_pos_anno,
+    polarity = 'positive',
+    ms1.match.ppm = 15,
+    column = T.column,
+    threads = T.threads,
+    database = hmdb_database0.0.3,
+    candidate.num = 2
+  )
+
+object_pos_anno <-
+  annotate_metabolites_mass_dataset(
+    object = object_pos_anno,
+    polarity = 'positive',
+    ms1.match.ppm = 15,
+    column = T.column,
+    threads = T.threads,
+    database = fiehn_hilic_database0.0.3,
+    candidate.num = 2
+  )
+
+object_pos_anno <-
+  annotate_metabolites_mass_dataset(
+    object = object_pos_anno,
+    polarity = 'positive',
+    ms1.match.ppm = 15,
+    column = T.column,
+    threads = T.threads,
+    database = RIKEN_PlaSMA_database0.0.1,
+    candidate.num = 2
+  )
+
+save(object_pos_anno,file = "object_pos_anno.rds")
--- README.md
+++ README.md
@@ -18,11 +18,59 @@ OS: `MacOS > 10.10`, `Win 7-11`, `Ubuntu 20.04`
 
 # Dependence
 
--   [TidyMass](https://www.tidymass.org/) package developed by [Dr. Xiaotao Shen](https://www.shenxt.info/)[<sup>1<sup/>](#refer-anchor-1)
+LC-MS data analysis framwork: [**TidyMass**](https://www.tidymass.org/) developed by [Dr. Xiaotao Shen](https://www.shenxt.info/). [@Citation](#refer-anchor-1)
+
+```r
+if(!require(remotes)){
+install.packages("remotes")
+}
+remotes::install_gitlab("tidymass/tidymass")
+```
+
+Metabolomics Downstream Analysis toolkits: [**MDAtoolkits**](https://github.com/ShawnWx2019/MDAtoolkits/tree/master) 
+
+```r
+## install from github
+suppressMessages(if (!require('MDAtoolkits')) install_github(repo = "ShawnWx2019/MDAtoolkits",ref = 'master'))
+## two functions from another package called IMOtoolkits. will be intergreted with MDAtoolkits.
+suppressMessages(if (!require('IMOtoolkits')) install_github(repo = "ShawnWx2019/IMOtoolkits"))
+```
+
+Untargeted metabolomics upstream analysis pipeline based on tidyMass: [**HPC-tidymass** ](https://github.com/ShawnWx2019/HPC_tidymass) 
+
+```r
+##>  you can download this pipeline from github or just clone the repo to your server
+##>  1.0 clone the repo to your server
+git clone https://github.com/ShawnWx2019/HPC_tidymass.git
+
+##> Run the initialization script to configure the runtime environment.
+
+cd HPC_tidymass && chmod +x init.sh && bash init.sh
+
+##> test
+hpc-runTidymass -h
+```
+
+# Step 1. Upstream analysis
+
+Here we refer to the process of obtaining the metabolomics data from LC-MS raw data to the generation of a metabolite accumulation matrix as <font color=blue>**「upstream data analysis data」**</font>. It includes processes such as <font color=green> 「raw data format conversion」, 「peak picking」, 「data cleaning」, and 「data normalization」</font>. In this pipeline, this part is typically performed by TidyMass or Compound Discoverer. If you have used other software for peak picking, you can refer to https://www.tidymass.org/start/create_mass_dataset/ to create a mass_dataset, which can then be used for subsequent analysis with TidyMass.
+
+## 1.1 Pseudotargeted metabolomics
+
+For pseudotargeted metabolomics, in the initial stage, HPLC experiments can be conducted using 5-10 QC (quality control) samples. Since the data volume is relatively small, and considering the completeness of commercial software databases, we recommend utilizing dedicated analysis software for peak detection and library searching. Alternatively, you can also opt to perform upstream analysis directly with TidyMass, followed by MRM (Multiple Reaction Monitoring) selection to generate a TQMS (Triple Quadrupole Mass Spectrometry) method.
+
+If using commercial software, it is recommended to follow the instructions provided in the software's manual for step-by-step analysis. If using TidyMass, it is advised to refer to the tutorial available at https://www.tidymass.org/start/ and follow the provided guidelines. Alternatively, you can also execute the test scripts provided by the pipeline.
+
+
+
+
+
 
 ## Reference
 
-::: {#refer-anchor-1}
-:::
+<div id=#refer-anchor-1></div>
 
--   [1] [Shen, X., Yan, H., Wang, C., Gao, P., Johnson, C.H. & Snyder, M.P. (2022) TidyMass an object-oriented reproducible analysis framework for LC--MS data. Nature Communications, 13, 4365.](https://www.nature.com/articles/s41467-022-32155-w)
+
+1. Shen, X., Yan, H., Wang, C., Gao, P., Johnson, C.H. & Snyder, M.P. (2022) TidyMass an object-oriented reproducible analysis framework for LC--MS data. Nature Communications, 13, 4365. DOI: [https://doi.org/10.1038/s41467-022-32155-w](https://www.nature.com/articles/s41467-022-32155-w)
+
+
